927c88327bba3a68fb3959e0dedcd1e4fc911326
Lúcia Duarte <analuciacfduarte@gmail.com>
2023-12-12 18:25:24 +0000

    day03 part1 refactor

--- day03/src/bin/part1.rs
+++ day03/src/bin/part1.rs
@@ -6,91 +6,79 @@ fn main() {
     dbg!(output);
 }
 
+#[derive(Debug)]
+struct SpecialChar {
+    line: i32,
+    positions: Vec<i32>,
+}
+
 fn part1(input: &str) -> i32 {
     let lines: Vec<_> = input.lines().collect();
 
-    let number_re: Regex = Regex::new(r"[0-9]+").unwrap();
-    let special_re: Regex = Regex::new(r"[^A-z.\s\d][\\\^]?").unwrap();
+    let mut special_chars: Vec<SpecialChar> = Vec::new();
 
     let mut parts: Vec<i32> = Vec::new();
 
-    let mut sum: i32 = 0;
+    // let mut sum: i32 = 0;
+
+    let num_re = Regex::new(r"[0-9]+").unwrap();
+    let special_re = Regex::new(r"[^A-z.\s\d][\\\^]?").unwrap();
 
     for (index, line) in lines.iter().enumerate() {
-        let index = i32::try_from(index).unwrap();
-        let numbers: Vec<_> = number_re.find_iter(line.trim()).map(|m| m).collect();
+        let result: Vec<_> = special_re.find_iter(line.trim()).map(|m| m).collect();
 
-        let mut special_chars: Vec<_> = Vec::new();
+        let mut chars_position: Vec<i32> = Vec::new();
 
-        if index < (lines.len() as i32 - 2) as i32 {
-            special_chars = special_re
-                .find_iter(lines[(index + 1) as usize].trim())
-                .map(|m| m)
-                .collect();
+        for mat in result {
+            chars_position.push(mat.start().try_into().unwrap())
         }
+
+        if chars_position.len() != 0 {
+            special_chars.push(SpecialChar {
+                line: index.try_into().unwrap(),
+                positions: chars_position,
+            });
+        }
+    }
+
+    for (index, line) in lines.iter().enumerate() {
+        let numbers: Vec<_> = num_re.find_iter(line.trim()).map(|m| m).collect();
 
         for number in numbers {
-            let interval = ((number.start() as i32 - 1), (number.end() as i32 + 1));
+            let interval = ((number.start() as i32 - 1), (number.end() as i32) + 1);
 
-            for special_char in &special_chars {
-                if special_char.start() as i32 >= interval.0
-                    && special_char.end() as i32 <= interval.1
-                {
-                    parts.push(number.as_str().parse::<i32>().unwrap());
-                }
-            }
-
-            if index > 0 {
-                let special_chars: Vec<_> = special_re
-                    .find_iter(lines[(index - 1) as usize].trim())
-                    .map(|m| m)
-                    .collect();
-
-                let interval = ((number.start() as i32 - 1), (number.end() as i32 + 1));
-
-                for special_char in special_chars {
-                    if special_char.start() as i32 >= interval.0
-                        && special_char.end() as i32 <= interval.1
-                    {
-                        parts.push(number.as_str().parse::<i32>().unwrap());
+            special_chars
+                .iter()
+                .filter(|s| {
+                    s.line == index.try_into().unwrap()
+                        || s.line == (index as i32) - 1
+                        || s.line == (index as i32) + 1
+                })
+                .for_each(|s| {
+                    for position in &s.positions {
+                        if position >= &interval.0 && position + 1 <= interval.1 {
+                            parts.push(number.as_str().parse::<i32>().unwrap())
+                        }
                     }
-                }
-            }
-
-            let special_chars: Vec<_> = special_re
-                .find_iter(lines[index as usize].trim())
-                .map(|m| m)
-                .collect();
-
-            let interval = ((number.start() as i32 - 1), (number.end() as i32 + 1));
-
-            for special_char in special_chars {
-                if special_char.start() as i32 >= interval.0
-                    && special_char.end() as i32 <= interval.1
-                {
-                    parts.push(number.as_str().parse::<i32>().unwrap());
-                }
-            }
+                });
         }
     }
-    for part in parts.clone() {
-        sum += part
-    }
 
-    println!("Sum:\n{:?}", parts);
-    sum
+    parts.iter().sum::<i32>()
+
+    // sum
 }
 
 #[cfg(test)]
+
 mod tests {
     use super::*;
     #[test]
-
     fn it_works() {
         let result = part1(
             "467..114..
             ...*......
-            ..35..633.
+            ..35..598.
             ......#...
             617*......
             .....+.58.
@@ -99,6 +87,6 @@ mod tests {
             ...$.*....
             .664.598..",
         );
-        assert_eq!(result, 4361)
+        assert_eq!(result, 4326)
     }
 }
